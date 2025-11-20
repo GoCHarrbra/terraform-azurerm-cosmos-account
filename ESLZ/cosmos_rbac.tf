ccd2bf9971630c1b94f9aacacf91b90ade19ea4b
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-20 14:42:39 -0500

    fix cosmos rbac

--- ESLZ/cosmos_rbac.tf
+++ ESLZ/cosmos_rbac.tf
@@ -1,22 +1,41 @@
-# All fields required; must be set in cosmos-rbac.tfvars
-variable "cosmos_rbac" {
-  description = "Settings to grant a principal data-plane access to Cosmos."
-  type = object({
-    # Object ID of the principal (UMI or Service Principal)
-    principal_object_id     = string
+# cosmos_rbac.tf
+#
+# Grants a principal (UMI/SPN/user) the built-in Cosmos DB
+# "Data Contributor" **data-plane** role for the account created
+# by module.cosmos.
 
-    # Role name to grant, e.g. "Cosmos DB Built-in Data Contributor"
-    cosmos_data_role_name   = string
+variable "cosmos_rbac" {
+  description = "Settings to grant Cosmos SQL data-plane role to a principal."
+  type = object({
+    # Object ID of the principal (UMI/SPN/user) in Entra ID
+    principal_id = string
   })
 }
 
-resource "azurerm_role_assignment" "cosmos_data_role" {
-  scope                = module.cosmos.account_id
-  role_definition_name = var.cosmos_rbac.cosmos_data_role_name
-  principal_id         = var.cosmos_rbac.principal_object_id
+# Well-known GUID for built-in "Cosmos DB Built-in Data Contributor"
+# This roleDefinition lives under the Cosmos account:
+#   <account_id>/sqlRoleDefinitions/0000...0002
+locals {
+  cosmos_builtin_data_contributor_guid = "00000000-0000-0000-0000-000000000002"
+
+  cosmos_data_contributor_role_id = "${module.cosmos.account_id}/sqlRoleDefinitions/${local.cosmos_builtin_data_contributor_guid}"
 }
 
-output "cosmos_data_role_id" {
-  description = "ID of the Cosmos data-plane role assignment."
-  value       = azurerm_role_assignment.cosmos_data_role.id
+resource "azurerm_cosmosdb_sql_role_assignment" "data_contributor" {
+  name = uuid()
+
+  resource_group_name = module.cosmos.rg_name
+  account_name        = module.cosmos.account_name
+
+  # Data-plane role definition & scope
+  role_definition_id = local.cosmos_data_contributor_role_id
+  scope              = module.cosmos.account_id
+
+  # Principal getting the data-plane permissions
+  principal_id = var.cosmos_rbac.principal_id
+}
+
+output "cosmos_data_contributor_assignment_id" {
+  description = "Cosmos SQL data-plane role assignment id."
+  value       = azurerm_cosmosdb_sql_role_assignment.data_contributor.id
 }
