1003f6af30b70c0ac375ccd8d24d49212d05aec0
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-21 18:27:03 -0500

    v0.6.0

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,25 @@
+# Useful outputs for downstream modules (and diagnostics/RBAC/PE)
 output "cosmos_account_id" {
   description = "Cosmos DB account resource ID."
-  value       = azurerm_cosmosdb_account.this.id
+  value       = module.cosmos.account_id
 }
 
 output "cosmos_account_name" {
   description = "Cosmos DB account name."
-  value       = azurerm_cosmosdb_account.this.name
+  value       = module.cosmos.account_name
+}
+
+output "cosmos_rg_name" {
+  description = "Resource group used for the Cosmos account."
+  value       = module.cosmos.rg_name
+}
+
+output "cosmos_location" {
+  description = "Location used for the Cosmos account."
+  value       = module.cosmos.location
+}
+
+output "cosmos_tags" {
+  description = "Tags applied to the Cosmos account."
+  value       = module.cosmos.tags
 }
