e93feb17d2e626ddd667e405891a33955e2cf6f7
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-24 17:56:45 -0500

    Refactor cosmos.tf variable and module definitions
    fix outputs

--- ESLZ/cosmos.tf
+++ ESLZ/cosmos.tf
@@ -85,27 +85,28 @@ module "cosmos" {
 # Useful outputs for downstream modules (and diagnostics/RBAC/PE)
 output "cosmos_account_id" {
   description = "Cosmos DB account resource ID."
-  value       = module.cosmos.account_id
+  value       = module.cosmos.cosmos_account_id
 }
 
 output "cosmos_account_name" {
   description = "Cosmos DB account name."
-  value       = module.cosmos.account_name
+  value       = module.cosmos.cosmos_account_name
 }
 
 output "cosmos_rg_name" {
   description = "Resource group used for the Cosmos account."
-  value       = module.cosmos.rg_name
+  value       = module.cosmos.cosmos_rg_name
 }
 
 output "cosmos_location" {
   description = "Location used for the Cosmos account."
-  value       = module.cosmos.location
+  value       = module.cosmos.cosmos_location
 }
 
 output "cosmos_tags" {
   description = "Tags applied to the Cosmos account."
-  value       = module.cosmos.tags
+  value       = module.cosmos.cosmos_tags
 }
+
 
 
