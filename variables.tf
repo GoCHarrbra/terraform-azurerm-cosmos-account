0cce949fa8a19b8330a6636d01f2837766e1e8c0
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-26 11:40:20 -0500

    Fix CKV Azure 132

--- variables.tf
+++ variables.tf
@@ -126,7 +126,8 @@ variable "databases" {
   }))
 }
 
-variable "key_based_metadata_write_access_enabled" {
+variable "access_key_metadata_writes_enabled" {
   description = "Whether key-based clients can modify metadata (DBs/containers). Set to false to restrict management-plane changes and satisfy CKV_AZURE_132."
   type        = bool
 }
+
